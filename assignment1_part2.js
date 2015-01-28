ec85a4f8b5fd53b968da29db1bc24e11c508b5ae
Tui Popenoe <tuipopenoe@gmail.com>
2015-01-27 23:48:42 -0600

    Assignment 1

---
+++ assignment1_part2.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+function listPrimes( nPrimes ) {
+    var primes = [];
+    for( var n = 2;  nPrimes > 0;  n++ ) {
+        if( isPrime(n) ) {
+            primes.push( n );
+            --nPrimes;
+        }
+    }
+    return primes;
+}
+
+function isPrime( n ) {
+    var max = Math.sqrt(n);
+    for( var i = 2;  i <= max;  i++ ) {
+        if( n % i === 0 )
+            return false;
+    }
+    return true;
+}
+
+var fs = require('fs');
+fs.writeFileSync('primes.txt', listPrimes(100));
